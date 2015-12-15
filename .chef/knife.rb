705e3c5e0f481648494e98df290f25e285ef38c6
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-12-15 21:31:24 +0900

    install nginx

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,8 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
-node_path        "nodes"
-role_path        "roles"
-environment_path "environments"
-data_bag_path    "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
-Chef::Config[:ssl_verify_mode] = :verify_peer if defined? ::Chef
+log_level                :info
+log_location             STDOUT
+node_name                'kazuya.matsumoto'
+client_key               '/Users/kazuya.matsumoto/desktop/chef_rails/rails_env_chef/.chef/kazuya.matsumoto.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://g-pc-5474.intra.gree-office.net:443'
+syntax_check_cache_path  '/Users/kazuya.matsumoto/desktop/chef_rails/rails_env_chef/.chef/syntax_check_cache'
---
+++ cookbooks/rails_env_cookbook/recipes/default.rb
@@ -0,0 +1,69 @@
+#
+# Cookbook Name:: rbenv
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# 必要なパッケージのインストール
+# http://docs.getchef.com/resource_package.html
+%w{git gcc openssl-devel readline-devel}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# --------------------------------------------------------
+# install ruby by rbenv
+# --------------------------------------------------------
+# rbenvのダウンロード
+# http://docs.getchef.com/resource_git.html
+git "/usr/local/rbenv" do
+  repository "https://github.com/sstephenson/rbenv.git"
+  revision "master"
+  action :sync
+end
+
+# プラグインディレクトリの作成
+# http://docs.getchef.com/resource_directory.html
+directory "/usr/local/rbenv/plugins" do
+  action :create
+end
+
+# ruby-buildプラグインのダウンロード
+git "/usr/local/rbenv/plugins/ruby-build" do
+  repository "https://github.com/sstephenson/ruby-build.git"
+  revision "master"
+  action :sync
+end
+
+# bashの環境設定を行うシェルスクリプトの設置 (先程作成したテンプレートファイルを利用)
+# http://docs.getchef.com/resource_template.html
+template "/etc/profile.d/rbenv.sh" do
+  source "rbenv.sh.erb"
+  action :create
+end
+
+template "/var/www/nginx-default/index.html" do
+  source "index.html"
+  action :create
+end
+
+
+# rubyのインストール
+# http://docs.getchef.com/resource_bash.html
+bash "install-ruby-with-rbenv" do
+  # code "" の中身が実行
+  code "source /etc/profile.d/rbenv.sh && rbenv install 2.2.2 && rbenv rehash"
+  action :run
+  not_if { File.exists?('/usr/local/rbenv/versions/2.2.2') }
+end
+
+# bundlerのインストール
+bash "install-bundler" do
+  code "source /etc/profile.d/rbenv.sh && rbenv global 2.2.2 && rbenv exec gem install bundler && rbenv rehash"
+  action :run
+end
+
