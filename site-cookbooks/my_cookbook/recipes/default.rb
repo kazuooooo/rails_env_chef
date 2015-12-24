99c6c55b43af6306ea8c563a7ae4c060db8a4c59
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-12-24 14:36:24 +0900

    secrets value encrypted

--- site-cookbooks/my_cookbook/recipes/default.rb
+++ site-cookbooks/my_cookbook/recipes/default.rb
@@ -6,14 +6,3 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-
-# rubyのバージョンとglobal設定
-rbenv_ruby '2.2.4'
-
-# nginxのserviceを設定
-service "nginx" do
-  # chef-clientがserviceをどのように扱うかを設定
-  supports :status => true, :restart => true, :reroad => true
-  # serviceをboot出来るようにしておく
-  action [:enable]
-end
--- site-cookbooks/my_cookbook/recipes/secrets.rb
+++ site-cookbooks/my_cookbook/recipes/secrets.rb
@@ -4,6 +4,15 @@ template "/etc/profile.d/secrets.sh" do
   action :create
 end
 
+# 値を飛ばす
+data_bag = Chef::EncryptedDataBagItem.load('foo','bar')
+naisyo_value = data_bag['naisyo_value']
+template "/etc/profile.d/secrets.sh" do
+  variables ({
+              :naisyo => naisyo_value
+            })
+end
+
 # secrets.shを読み込み
 bash "load secrets.sh" do
   code "source /etc/profile.d/secrets.sh"
