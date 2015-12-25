bf580de27899d05f865f5119628cef2f9f58eb41
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-12-25 12:29:38 +0900

    nginx-site add

--- site-cookbooks/my_cookbook/recipes/nginx_conf.rb
+++ site-cookbooks/my_cookbook/recipes/nginx_conf.rb
@@ -1,17 +1,9 @@
-# nginxのserviceを設定
-service "nginx" do
-  # chef-clientがserviceをどのように扱うかを設定
-  supports :status => true, :restart => true, :reroad => true
-  # serviceをboot出来るようにしておく
-  action [:enable]
-end
-
-# nginxのテンプレートファイルを配置
-template "nginx.conf" do
-  path "/etc/nginx/nginx.conf"
+template "#{node["nginx"]["dir"]}/sites-available/conf" do
   source "nginx.conf.erb"
+  mode 0644
   owner "root"
   group "root"
-  mode 0644
   notifies :reload, 'service[nginx]'
 end
+
+nginx_site "conf"
