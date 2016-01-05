a08a4b7de2507035531ce28562408c149d6b0dc2
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2016-01-05 10:37:51 +0900

    cosme

--- site-cookbooks/my_cookbook/spec/unit/recipes/nginx_conf_spec.rb
+++ site-cookbooks/my_cookbook/spec/unit/recipes/nginx_conf_spec.rb
@@ -2,10 +2,11 @@ require_relative '../spec_helper'
 
 describe 'my_cookbook::nginx_conf' do
   let(:chef_run) do
+
     stub_command("which nginx").and_return('/usr/local/bin/nginx')
+
     ChefSpec::SoloRunner.new do |node|
       node.set["nginx"]["dir"] = "/etc/nginx"
-      #  node.set["nginx"]["port"] = "8080"
     end.converge(described_recipe)
   end
 
