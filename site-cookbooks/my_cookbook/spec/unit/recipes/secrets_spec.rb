6eeca1fa6e2af5bf6ba8cbba731e7ebd24d267cd
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2016-01-05 14:50:25 +0900

    make secrets_spec work

--- site-cookbooks/my_cookbook/spec/unit/recipes/secrets_spec.rb
+++ site-cookbooks/my_cookbook/spec/unit/recipes/secrets_spec.rb
@@ -1,8 +1,12 @@
-require 'pry'
 require_relative '../spec_helper'
 
 describe 'my_cookbook::secrets' do
-
+  before do
+    allow(Chef::EncryptedDataBagItem).to receive(:load).with('foo', 'bar').and_return({
+      id: "bar",
+      naisyo_value: "test_naisyo_value"
+    })
+  end
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   it "create /etc/profile.d/secrets.sh" do
@@ -10,38 +14,13 @@ describe 'my_cookbook::secrets' do
   end
 
   it "get naisyo value" do
-    allow(Chef::EncryptedDataBagItem).to receive(:load).with('foo', 'bar').and_return({
-      id: "bar",
-      naisyo_value: "test_naisyo_value"
-    })
     data_bag = Chef::EncryptedDataBagItem.load('foo','bar')
     naisyo_value = data_bag[:naisyo_value]
     expect(naisyo_value).to eq("test_naisyo_value")
   end
 
-  # it "load secrets.sh by bash" do
-  #   expect(chef_run).to run_bash("source /etc/profile.d/secrets.sh")
-  # end
+  it "load secrets.sh" do
+    expect(chef_run).to run_bash("load secrets.sh")
+  end
 
 end
-
-# # secrets.shを作成
-# template "/etc/profile.d/secrets.sh" do
-#   source "secrets.sh.erb"
-#   action :create
-# end
-
-# # 値を飛ばす
-# data_bag = Chef::EncryptedDataBagItem.load('foo','bar')
-# naisyo_value = data_bag['naisyo_value']
-# template "/etc/profile.d/secrets.sh" do
-#   variables ({
-#               :naisyo => naisyo_value
-#             })
-# end
-
-# # secrets.shを読み込み
-# bash "load secrets.sh" do
-#   code "source /etc/profile.d/secrets.sh"
-#   action :run
-# end
